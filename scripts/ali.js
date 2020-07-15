b65e532ab6e4c2fe957e9157e308877c9805caf3
galaxyeagle <raman.butta@live.in>
2020-07-15 19:21:41 +0530

    strategic changes to ali file

---
+++ scripts/ali.js
@@ -0,0 +1,17 @@
+
+
+
+function expand(a)
+{
+	x = document.getElementById(a);
+	if (x.style.display === 'none')
+		x.style.display = 'block';	
+	else
+		x.style.display = 'none';
+}
+
+function change()
+	{
+		v = document.getElementById("mybutton"); 
+		v.innerHTML= "COMMENT BELOW or DROP ME AN EMAIL AT raman.butta.nitdgp@gmail.com";
+	}
